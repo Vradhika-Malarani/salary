8f7979e215f32e6efc3f033b9ac38afa16210320
unknown <siddiqaboobakkarsiddiq.com>
2025-01-10 16:35:50 +0530

    Login page

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './Home'
-
+import Login from './Login'
 const Router = () => {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Home/>} />
+            <Route path='/Home' element={<Home/>} />
+            <Route path='/' element={<Login/>} />
         </Routes>
     </BrowserRouter>
   )
